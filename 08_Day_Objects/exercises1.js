80e1ece748738a684c160724db394c0623db44f9
Büşra Çoğul <cogulbusra@gmail.com>
2024-01-25 15:56:40 +0300

    08_Day_exercises1

---
+++ 08_Day_Objects/exercises1.js
@@ -0,0 +1,29 @@
+// Exercises 1
+let dog = {};
+
+// Exercises 2
+console.log(dog);
+
+// Exercises 3
+dog.name = 'Mars';
+dog.legs = 4;
+dog.color = 'Brown';
+dog.age = 2;
+
+dog.barkProperty = function () {
+    return 'woof woof';
+}
+console.log(dog);
+
+// Exercises 4
+console.log(dog.name);
+console.log(dog.legs);
+console.log(dog.color);
+console.log(dog.age);
+console.log(dog.barkProperty);
+
+// Exercises 5
+dog.breed = 'golden';
+dog.getDogInfo = 'very sweet and cute dog';
+console.log(dog.breed);
+console.log(dog.getDogInfo);
